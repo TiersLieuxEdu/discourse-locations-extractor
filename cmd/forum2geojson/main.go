e42a0de06746903c4de3ae3c0c89da0529f1ba6a
Loïc Fejoz <loic@fejoz.net>
2019-10-08 21:25:40 +0200

    fix(forum2geojson): lower tag as property

--- cmd/forum2geojson/main.go
+++ cmd/forum2geojson/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "log"
-  "sort"
-  gj "github.com/kpawlik/geojson"
-  "github.com/TiersLieuxEdu/discourse-locations-extractor/pkg/forum"
+	"encoding/json"
+	"fmt"
+	"log"
+	"sort"
+	"strings"
+
+	"github.com/TiersLieuxEdu/discourse-locations-extractor/pkg/forum"
+	gj "github.com/kpawlik/geojson"
 )
 
-// Marshal object to json string representation
+// MarshalIndent object to json string representation
 func MarshalIndent(object interface{}, indent string) (data string, err error) {
 	if t, err := json.MarshalIndent(object, "", indent); err != nil {
 		data = ""
@@ -21,28 +23,31 @@ func MarshalIndent(object interface{}, indent string) (data string, err error) {
 
 func main() {
 
-  topics := forum.GetTopics()
-  sort.SliceStable(topics, func(i, j int) bool {
-    return topics[i].Id < topics[j].Id
-  })
-  fc := gj.NewFeatureCollection([]*gj.Feature {})
-  for _, value := range topics {
-    log.Printf("%s...\n", value.Title)
-    info := forum.GetInformations(value)
+	topics := forum.GetTopics()
+	sort.SliceStable(topics, func(i, j int) bool {
+		return topics[i].Id < topics[j].Id
+	})
+	fc := gj.NewFeatureCollection([]*gj.Feature{})
+	for _, value := range topics {
+		log.Printf("%s...\n", value.Title)
+		info := forum.GetInformations(value)
 
-    if info.Lat == 0 || info.Long == 0 {
-      log.Println("Coordinates not set. Skipping.")
-      continue
-    }
-    p := gj.NewPoint(gj.Coordinate{gj.CoordType(info.Long), gj.CoordType(info.Lat)})
-    props := map[string]interface{}{"name": info.Name, "forum": info.Forum, "site": info.WebSite}
-    f2 := gj.NewFeature(p, props, nil)
-    fc.AddFeatures(f2)
-  }
+		if info.Lat == 0 || info.Long == 0 {
+			log.Println("Coordinates not set. Skipping.")
+			continue
+		}
+		p := gj.NewPoint(gj.Coordinate{gj.CoordType(info.Long), gj.CoordType(info.Lat)})
+		props := map[string]interface{}{"name": info.Name, "forum": info.Forum, "site": info.WebSite, "tags": info.Tags}
+		for _, aTag := range info.Tags {
+			props[strings.ToLower(aTag)] = true
+		}
+		f2 := gj.NewFeature(p, props, nil)
+		fc.AddFeatures(f2)
+	}
 
-  if gjstr, err := MarshalIndent(fc, "  "); err != nil {
-      panic(err)
-  } else {
-      fmt.Println(gjstr)
-  }
+	if gjstr, err := MarshalIndent(fc, "  "); err != nil {
+		panic(err)
+	} else {
+		fmt.Println(gjstr)
+	}
 }
